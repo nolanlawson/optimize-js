9dcd0f569b4cfbcab19342e27bc18cd767577ea1
Nolan Lawson <nolan.lawson@gmail.com>
2016-09-17 16:09:56 -0700

    add ember style

---
+++ test/cases/ember-style/input.js
@@ -0,0 +1,8 @@
+(function (e, t) {
+  "use strict";
+  "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function (e) {
+    if (!e.document)throw new Error("jQuery requires a window with a document");
+    return t(e)
+  } : t(e)
+})(true), function () {
+}();
