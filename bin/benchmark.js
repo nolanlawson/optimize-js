c62d50ab4b080801a5304660580cd6e38ba3539a
Nolan Lawson <nolan.lawson@gmail.com>
2016-09-17 13:41:17 -0700

    start on benchmarks

---
+++ bin/benchmark.js
@@ -0,0 +1,24 @@
+var Benchmark = require('benchmark')
+var fs = require('fs')
+var optimizeJs = require('..')
+var path = require('path')
+
+var benchmarks = fs.readdirSync('./benchmarks')
+
+benchmarks.forEach(function (benchmark) {
+  var unoptimized = fs.readFileSync(path.join('benchmarks', benchmark, 'index.js'), 'utf8')
+  var optimized = optimizeJs(unoptimized)
+
+  var suite = new Benchmark.Suite()
+  suite.add(benchmark, function() {
+    eval(unoptimized)
+  }).add(benchmark + ' w/ optimize-js', function() {
+    eval(optimized)
+  }).on('cycle', function(event) {
+    console.log(String(event.target))
+  }).on('error', function (err) {
+    console.log(err)
+  }).on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  }).run()
+})
