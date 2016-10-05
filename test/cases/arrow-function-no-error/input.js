a9e4e0ead16a06a9627ef77d4051b3cbe0bc8bd4
Si Thu Latt <ideaterminal@gmail.com>
2016-10-04 22:13:02 -0700

    use estree-walker to properly parse es6
    remove comment

---
+++ test/cases/arrow-function-no-error/input.js
@@ -0,0 +1,7 @@
+// examples taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+var func = x => x * x;
+var func = (x, y) => { return x + y; };
+
+setInterval(() => {
+  this.age++;
+}, 1000);
