cd00debb2c3e86f2c51a64c04d1f6554966618b0
Sasha Aickin <xander76@yahoo.com>
2016-11-01 09:15:36 -0700

    Webpack-specific fix for issue #7 (#33)
    * Fix for #7: Not as optimized for Webpack as it could be. Adds ability to wrap functions that are elements of an array literal in an argument list
    
    * Added a few negative case tests in response to code review.
    
    * Modified the array literal logic to very specifically track with webpack patterns rather than all array literal function parameters.

---
+++ test/cases/webpack-style-multi-element/input.js
@@ -0,0 +1,9 @@
+!function(o){
+  return o[0]();
+}([function(o,r,t){
+  console.log("webpack style element 0");
+},function(o,r,t){
+  console.log("webpack style element 1");
+},function(o,r,t){
+  console.log("webpack style element 2");
+}]);
